921b19aaac3358d0ab326e188f3568ea097ec3d2
Youssef Ragab <ragab.youssef95@gmail.com>
2024-12-12 19:17:54 -0500

    gix scroll opacity bug

--- script.js
+++ script.js
@@ -8,18 +8,27 @@ document.addEventListener("DOMContentLoaded", () => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add(toggleClass);
+          const alibiDesc = document.getElementById("alibi-desc");
+          const guilsProDesc = document.getElementById("guildsPro-desc");
+
           const alibiStack = document.getElementById("alibi-stack");
           const guildsproStack = document.getElementById("guilsPro-stack");
           if (entry.target.classList[1] === "alibi") {
             guildsproStack.style.display = "none";
             alibiStack.style.display = "inline";
+            guilsProDesc.classList.remove("active");
+            alibiDesc.classList.add("active");
           } else if (entry.target.classList[1] === "guildsPro") {
             alibiStack.style.display = "none";
             guildsproStack.style.display = "inline";
+
+            guilsProDesc.classList.add("active");
+            alibiDesc.classList.remove("active");
           }
-        } else {
-          entry.target.classList.remove(toggleClass);
         }
+        // else {
+        //   entry.target.classList.remove(toggleClass);
+        // }
       });
     }, observerOptions);
 
